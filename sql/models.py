5936b5aa34aa52758cff8bb4193125ea2d786d9c
Jan Lana <lana.jan@gmail.com>
2020-02-12 01:44:32 +0100

    pridana data

--- sql/models.py
+++ sql/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Battle(models.Model):
+    name = models.CharField(max_length=20)
+    date = models.DateField()
+
+class ShipClass(models.Model):
+    name = models.CharField(max_length=50)
+    typ = models.CharField(max_length=2)
+    country = models.CharField(max_length=20)
+    numGuns = models.IntegerField()
+    bore = models.FloatField()
+    displacement = models.IntegerField()
+
+class Ship(models.Model):
+    name = models.CharField(max_length=50)
+    cls = models.ForeignKey(ShipClass, on_delete=models.CASCADE)
+    launched = models.IntegerField()
+
+class Outcome(models.Model):
+    ship = models.ForeignKey(Ship, on_delete=models.CASCADE)
+    battle = models.ForeignKey(Battle, on_delete=models.CASCADE)
+    outcome = models.CharField(max_length=10)
